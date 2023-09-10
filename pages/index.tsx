81490a64d3f0deb8a54343f5fec4e2ea5bfa354e
seorang42 <seorang42@naver.com>
2023-09-10 15:23:53 +0900

    4.8 More Modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,29 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <form className="flex flex-col space-y-2 p-5 focus-within:bg-blue-50">
+    <div className="flex flex-col space-y-2 p-5">
+      <details className="select-none open:text-white open:bg-indigo-500">
+        <summary className="select-none cursor-pointer">
+          What is my fav. food.
+        </summary>
+        <span>김치</span>
+      </details>
+      <ul className="list-disc marker:text-teal-500">
+        <li>hi</li>
+        <li>hi</li>
+        <li>hi</li>
+      </ul>
       <input
-        type="text"
-        required
-        placeholder="Username"
-        className="border p-1 peer border-gray-400 rounded-md"
+        type="file"
+        className="file:hover:text-purple-400 file:hover:bg-white file:hover:border-purple-400 file:hover:border file:cursor-pointer file:transition file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400"
       />
-      <span className="hidden peer-invalid:block peer-invalid:text-red-500">
-        This input is invalid
-      </span>
-      <span className="hidden peer-hover:block peer-valid:text-amber-500">
-        Hello
-      </span>
-      <input type="submit" value="Login" className="bg-white" />
-    </form>
+      <p className="first-letter:text-7xl first-letter:hover:text-purple-400">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+        corporis, alias minima maiores ut debitis temporibus fugiat. Rerum
+        inventore aspernatur impedit nostrum eaque? Amet velit ducimus suscipit
+        rem, optio distinctio.
+      </p>
+    </div>
   );
 };
 
