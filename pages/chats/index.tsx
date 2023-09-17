6b9bb01500888f93d10fe17ab0466c41ed68f122
seorang42 <seorang42@naver.com>
2023-09-17 18:16:31 +0900

    5.13 Streams

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -4,7 +4,10 @@ const Chats: NextPage = () => {
   return (
     <div className="py-10 divide-y">
       {[1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div className="px-4 flex cursor-pointer py-3 items-center space-x-3">
+        <div
+          key={i}
+          className="px-4 flex cursor-pointer py-3 items-center space-x-3"
+        >
           <div className="w-12 h-12 rounded-full bg-slate-300" />
           <div>
             <p className="text-gray-700">Steve Jebs</p>
