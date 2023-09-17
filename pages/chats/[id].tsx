2c67037b72607610f5d633e79362f395dc40fb8a
seorang42 <seorang42@naver.com>
2023-09-17 17:00:39 +0900

    5.10 Profile

--- pages/chats/[id].tsx
+++ pages/chats/[id].tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const ChatDetail: NextPage = () => {
   return (
-    <div className="px-4 py-10 space-y-4">
+    <div className="px-4 py-10 pb-16 space-y-4">
       <div className="flex items-start space-x-2">
         <div className="w-8 h-8 rounded-full bg-slate-400" />
         <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
