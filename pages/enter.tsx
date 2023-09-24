ea6bac72e09229dc28927a3e7c34d3316e44ee62
seorang42 <seorang42@naver.com>
2023-09-24 16:42:59 +0900

    8.3 Clean Code part Two

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -23,9 +23,10 @@ export default function Enter() {
     reset();
     setMethod("phone");
   };
-  const onValid = (data: EnterForm) => {
-    enter(data);
+  const onValid = (validForm: EnterForm) => {
+    enter(validForm);
   };
+  console.log(loading, data, error);
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
