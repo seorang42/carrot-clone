4f765c9458dd53a852d89e0eb67f214ea112e209
seorang42 <seorang42@naver.com>
2023-10-02 18:06:14 +0900

    9.5 Sending Email

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import client from "@/libs/server/client";
 import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import { Twilio } from "twilio";
+import smtpTransport from "@/libs/server/email";
 
 const twilioClient = new Twilio(
   process.env.TWILIO_SID,
@@ -40,6 +41,28 @@ async function handler(
       body: `Your login token is ${payload}`,
     });
     console.log(message);
+  }
+  if (email) {
+    const mailOptions = {
+      from: process.env.MAIL_ID,
+      to: email,
+      subject: "Nomad Carrot Authentication Email",
+      text: `Authentication Code : ${payload}`,
+    };
+    const result = await smtpTransport.sendMail(
+      mailOptions,
+      (error, responses) => {
+        if (error) {
+          console.log(error);
+          return null;
+        } else {
+          console.log(responses);
+          return null;
+        }
+      }
+    );
+    smtpTransport.close();
+    console.log(result);
   }
   return res.json({
     ok: true,
