a9d73686a9271846cc7078019c09cf0054bdb25d
seorang42 <seorang42@naver.com>
2023-10-03 19:13:36 +0900

    9.8 Profile Handler

--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -15,21 +15,17 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { token } = req.body;
-  const exists = await client.token.findUnique({
-    where: {
-      payload: token,
-    },
+  console.log(req.session.user);
+  const profile = await client.user.findUnique({
+    where: { id: req.session.user?.id },
   });
-  if (!exists) return res.status(404).end();
-  req.session.user = {
-    id: exists.userId,
-  };
-  await req.session.save();
-  res.status(200).end();
+  res.json({
+    ok: true,
+    profile,
+  });
 }
 
-export default withIronSessionApiRoute(withHandler("POST", handler), {
+export default withIronSessionApiRoute(withHandler("GET", handler), {
   cookieName: "carrotSession",
   password:
     "91825693716513985713985612129856419826591836589137598112458612912556243198173132583169",
