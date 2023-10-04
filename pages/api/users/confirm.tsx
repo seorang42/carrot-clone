2bed19a0e8ff7c3cde4b17ae13f42a27bfd904ba
seorang42 <seorang42@naver.com>
2023-10-04 11:32:28 +0900

    10.1 Protected Handlers

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -26,4 +26,10 @@ async function handler(
   res.json({ ok: true });
 }
 
-export default withApiSession(withHandler("POST", handler));
+export default withApiSession(
+  withHandler({
+    method: "GET",
+    handler,
+    isPrivate: false,
+  })
+);
