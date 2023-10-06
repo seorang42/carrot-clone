95f59c539d8cabe4d838af9c14cb12e150bf0f78
seorang42 <seorang42@naver.com>
2023-10-06 19:21:41 +0900

    11.6 Favorite Products

---
+++ pages/api/products/[id]/fav.ts
@@ -0,0 +1,50 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@/libs/server/client";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { withApiSession } from "@/libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+  const alreadyExist = await client.fav.findFirst({
+    where: {
+      productId: Number(id),
+      userId: user?.id,
+    },
+  });
+  if (alreadyExist) {
+    await client.fav.delete({
+      where: {
+        id: alreadyExist.id,
+      },
+    });
+  } else {
+    await client.fav.create({
+      data: {
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+        product: {
+          connect: {
+            id: Number(id),
+          },
+        },
+      },
+    });
+  }
+  res.json({ ok: true });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["POST"],
+    handler,
+  })
+);
